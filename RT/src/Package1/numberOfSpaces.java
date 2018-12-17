5058a00d2f9ff517121b403a326b5ee74de0eb39
vivek <vivekrt90@gmail.com>
2018-12-17 15:49:53 -0500

    Number of spaces in sentence

---
+++ RT/src/Package1/numberOfSpaces.java
@@ -0,0 +1,24 @@
+package Package1;
+
+public class numberOfSpaces 
+{
+static int i,word=0,temp;
+private static numberOfSpaces SpaceCount;
+	   public static void main (String args[])
+{
+	temp=SpaceCount.spacecount("Count the number of spaces in this sentence"); 
+	System.out.println("The number of white spaces in the String are :  "+word);
+	               }   
+	   static int spacecount(String s)
+	   
+	   {
+	       for(i=0,word=0;i<s.length();i++)
+	       {
+	           char ch=s.charAt(i);
+	           if(ch==' ')
+	           word++;
+	       }
+	       return word;
+	   }
+	   
+	}
